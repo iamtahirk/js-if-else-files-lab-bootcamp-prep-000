873302358b05aa2fd9402c7f85741543b93ddcdf
Muhammad Tahir Khan <muhammadtahirk@gmail.com>
2018-01-12 05:52:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 var name = "Susan"
 var height = "74"
 
+/*
 if (name === "Susan") {
   console.log("Name match found!");
 } else {
   console.log("Sorry, the name doesn't match!");
 }
+*/
 
 // Don't worry about this
 module.exports = { name, height
